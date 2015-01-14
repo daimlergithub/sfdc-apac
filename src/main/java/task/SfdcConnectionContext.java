6b198ec71eb8433c0f927b48275714198403d6f2
Falk Lehmann <falk.lehmann.bp@nttdata.com>
2015-01-14 13:53:29 +0800

    Adding stuff for EPA tabs.

---
+++ src/main/java/task/SfdcConnectionContext.java
@@ -0,0 +1,37 @@
+/*
+ *  Daimler CRM - Extension Platform
+ */
+package task;
+
+import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.soap.metadata.MetadataConnection;
+
+/**
+ * SfdcConnectionContext
+ *
+ * @author  XLEHMF
+ */
+public class SfdcConnectionContext
+{
+
+  private EnterpriseConnection eConnection;
+  private MetadataConnection mConnection;
+  
+  public SfdcConnectionContext(EnterpriseConnection eConnection, MetadataConnection mConnection)
+  {
+    this.eConnection = eConnection;
+    this.mConnection = mConnection;
+  }
+
+  public EnterpriseConnection getEConnection()
+  {
+    return eConnection;
+  }
+
+  public MetadataConnection getMConnection()
+  {
+    return mConnection;
+  }
+
+}
+
