33565c80072e93f9515aac419dd929715d56e13d
Falk Lehmann <falk.lehmann.bp@nttdata.com>
2015-03-18 03:58:48 +1100

    Fixed Sonar.

--- runner/sonar-run-projects.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for dir in /opt/sonar-projects/*; do
-        if [ -f $dir/sonar-project.properties ]; then
-                cd $dir && ant -f ../antbuild.xml analyse
-        fi
-done
-
